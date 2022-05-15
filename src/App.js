2c25b892d275b8f6c29471f11ad03354c4fd9703
konok101 <rockykonok@gmail.com>
2022-05-16 03:14:18 +0600

    state component done

--- src/App.js
+++ src/App.js
@@ -1,33 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products= [
-    {name: 'laptop', price:'324333'},
-    {name: 'phone', price:'994333'},
-    {name: 'Watch', price:'34333'},
-    {name: 'Bike', price:'24333'},
-  ]
+ 
   return (
     <div className="App">
-      {
-        products.map(product=> <Product name={product.name} price={product.price}></Product>)
-      }
-{/*      <Product name='konk'></Product>
-     <Product name='konk'></Product>
-     <Product name='konk'></Product>
-     <Product name='konk'></Product> */}
+      <Counter></Counter>
+     
     </div>
   );
 }
 
+function Counter(){
+  const [count, setCount]= useState(22);
+/* 
+  const increaseCount =()=>{
+    const newCount= count + 1;
+    setCount(newCount); */
 
-function Product(props){
+  const increaseCount =()=>{
+    
+    setCount(count+1);
+  }
+  const DecreaseCount =()=>{
+    
+    setCount(count-1);
+  }
   return(
-    <div className='product'>
-      <h3>Name:{props.name} </h3>
-      <p>Price: {props.price}</p>
+    <div>
+      <h1>count:{count} </h1>
+      <button onClick={increaseCount} >Increase</button>
+      <button onClick={DecreaseCount}>Decrease</button>
     </div>
   )
 }
+
+ 
 export default App;
