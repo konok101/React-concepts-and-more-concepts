16576373bbb098acb766e7896564f1aa347fdf8e
konok101 <rockykonok@gmail.com>
2022-05-17 00:51:15 +0600

    api data load in react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
  
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUser></ExternalUser>
      
     </div>
   );
 }
 
+ 
+
+
+function ExternalUser(){
+  const [Use , setUse]= useState([]);
+
+  useEffect( () =>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data=> setUse(data))
+
+  }, []);
+  return (
+    <div>
+         <h2>External user</h2>
+    {
+      Use.map(user=> <User name={user.name} email={user.email}></User>)
+    }
+     
+  
+    </div>
+  )
+ 
+}
+
+
+function User(props){
+  return (
+    <div  style={{border: '2px solid red', margin: '20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
 function Counter(){
   const [count, setCount]= useState(22);
-/* 
-  const increaseCount =()=>{
-    const newCount= count + 1;
-    setCount(newCount); */
+ 
 
   const increaseCount =()=>{
     
